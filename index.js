bd96baef488d95378f9d8f1c6f9b8d464ec0dcc8
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-05 09:23:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Nyambura", "Florence", "Grace"];
+const cardMsg = [];
+function writeCards(names, event){
+    for( let i = 0; i < names.length; i++){
+        cardMsg.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardMsg
+}
+
+// countdown//
+function countDown(){
+    let number = 10;
+    while(number >= 0) {
+        console.log(number)
+        number--
+    }
+}
+
